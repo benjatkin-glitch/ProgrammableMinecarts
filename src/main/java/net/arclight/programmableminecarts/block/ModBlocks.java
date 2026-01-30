f2b5482cc65583eec90ede6a7b63fe377bc89098
Arclight <benjatkin@gmail.com>
2026-01-30 18:10:53 +0000

    Added lithium block

--- src/main/java/net/arclight/programmableminecarts/block/ModBlocks.java
+++ src/main/java/net/arclight/programmableminecarts/block/ModBlocks.java
@@ -1,16 +1,21 @@
 package net.arclight.programmableminecarts.block;
 
 import net.arclight.programmableminecarts.ProgrammableMinecarts;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    public static final Block LITHIUM_BLOCK = registerBlock("lithium_block", new Block(AbstractBlock.Settings.create().strength(2f)));
+    public static final Block LITHIUM_BLOCK = registerBlock("lithium_block",
+            new Block(AbstractBlock.Settings.create().strength(2f)
+                    .requiresTool().sounds(BlockSoundGroup.METAL)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -25,5 +30,10 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         ProgrammableMinecarts.LOGGER.info("Registering Mod Blocks for " + ProgrammableMinecarts.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.LITHIUM_BLOCK);
+        });
+
     }
 }
