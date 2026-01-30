afeb10e42bb2ab6c2838457f2f97b0f2eb25a24a
Arclight <benjatkin@gmail.com>
2026-01-30 16:58:00 +0000

    Initial commit

---
+++ src/main/java/net/arclight/programmableminecarts/ProgrammableMinecartsClient.java
@@ -0,0 +1,10 @@
+package net.arclight.programmableminecarts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProgrammableMinecartsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
